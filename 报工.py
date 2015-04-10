763bda66a43e83c39938d621a394dc3a9bea060e
machao23 <274445541@qq.com>
2015-04-10 14:55:15 +0800

    遵守PEP8规范

--- 报工.py
+++ 报工.py
@@ -1,4 +1,4 @@
-#encoding=cp936
+# encoding=cp936
 import ConfigParser
 import datetime
 import random
@@ -6,132 +6,157 @@ import re
 import requests
 from BeautifulSoup import BeautifulSoup
 import PAMIE
-import sys
 
 s = requests.session()
-bizTravelID = "1321_16645" #出差项目编号，报工页面写死的
+bizTravelID = "1321_16645"  # 出差项目编号，报工页面写死的
 WORK_HOURS = 7.9
 OT_WORK_HOURS = 4
 NORMAL_TYPE = 0
 OT_TYPE = 1
 
-#读取配置文件
+# 读取配置文件
 config = ConfigParser.ConfigParser()
 config.read("config.ini")
 userName = config.get("global", "userName")
 passWord = config.get("global", "passWord")
 projectID = config.get("global", "projectID")
-projectNames = config.get("global", "projectName").split(',')
-projectCnt = len(projectNames)
+project_names = config.get("global", "projectName").split(',')
+projectCnt = len(project_names)
 host = config.get("global", "host")
 holidays = config.get("global", "holidays").split(',')
-inputIDs = []
-
+input_ids = []
 base_form = {}
 
-def getLastWeekDate():
-	global startDate
-	global endDate
-	global base_form
 
-	today = datetime.date.today()
-	startDate = today + datetime.timedelta(-7 - today.weekday()) #获取上周一的日期
-	endDate = startDate + datetime.timedelta(6)
-	base_form = {'startDate': startDate, 'endDate': endDate}
-	print "报工开始日期: " + str(startDate) +  " 报工结束日期: " + str(endDate)
-	
+def get_last_week_date():
+    global startDate
+    global endDate
+    global base_form
 
-def sendToServer(path, form_data, desc="undefined", params=""):
-    response = s.post(host + path, data = form_data, params = params)
+    today = datetime.date.today()
+    startDate = today + datetime.timedelta(-7 - today.weekday())  # 获取上周一的日期
+    endDate = startDate + datetime.timedelta(6)
+    base_form = {'startDate': startDate, 'endDate': endDate}
+    print u"报工开始日期: " + str(startDate) + u" 报工结束日期: " + str(endDate)
+
+
+def send_to_server(path, form_data, desc="undefined", params=None):
+    response = s.post(host + path, data=form_data, params=params)
     if cmp(desc, "undefined") == 0:
         return response
 
     if cmp(response.content, "success") == 0:
         print desc + u"成功"
-    elif response.content == "PasswordOutOfDate": #登陆密码超时
-        form_data = {'loginName': userName,
+    elif response.content == "PasswordOutOfDate":  # 登陆密码超时
+        form_data = {
+            'loginName': userName,
             'oldPassword': passWord,
             'password': passWord,
             'password1': passWord,
-            'action':'resetPassword',
+            'action': 'resetPassword',
         }
-        sendToServer('forgetPasswordAction.do', form_data, '重置密码');
+        send_to_server('forgetPasswordAction.do', form_data, u'重置密码')
     else:
         print desc + "失败"
         print response.content.decode('gbk')
         exit()
 
-#登录
+
+# 登录
 def login():
-	login_data = {'loginName': userName,
-		'password': passWord,
-		'action':'checkOnline',
-	}
-	sendToServer('loginAction.do', login_data, u"登陆")
+    login_data = {
+        'loginName': userName,
+        'password': passWord,
+        'action': 'checkOnline',
+    }
+    send_to_server('loginAction.do', login_data, u"登陆")
 
-#增加工作项
-def addProject(projectName):
-    form_data={'action':'addmyworkdes',
-        'taskName': projectName,
-        'projectID':projectID}
+
+# 增加工作项
+def add_project(project_name):
+    form_data = {
+        'action': 'addmyworkdes',
+        'taskName': project_name,
+        'projectID': projectID}
     form_data.update(base_form)
-    sendToServer('mywork/timesheet/addMyWorkDesAction.do', form_data, "添加项目" + projectName)
+    send_to_server('mywork/timesheet/addMyWorkDesAction.do', form_data, "添加项目" + project_name)
 
-#添加出差
-def addBizTravel():
-	xml_data='''<?xml version="1.0" encoding="gb2312"?>
-	<TreeTable xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="TreeTable.xsd" high="20" width="125" color="black" detailable="true" standardmenu="true">
-	<TableData>
-	<RowData><name>出差（正常栏填0.1）</name><NodeData nodeid="T_1321_16645_4408" nodetypeid="1321_16645_4408" image="29" />
-	<CellData><Cell colid="ck" type="boolean">True</Cell><Cell colid="t1" /><Cell colid="t2" /></CellData></RowData>
-	</TableData></TreeTable>'''
-	params = {'startDate': startDate, 'endDate': endDate}
-	sendToServer('mywork/timesheet/addMyTaskAction.do', xml_data, "添加出差", params)
 
-def getWeekOfDay(desc):
-    weekOfDay = {u'星期一': '0',
-                 u'星期二': '1',
-                 u'星期三': '2',
-                 u'星期四': '3',
-                 u'星期五': '4',
-                 u'星期六': '5',
-                 u'星期天': '6',
-                 }
-    return weekOfDay.get(desc[:3])
+# 添加出差
+def add_biz_travel():
+    xml_data = (
+        '<?xml version="1.0" encoding="gb2312"?>'
+        '<TreeTable xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
+        'xsi:noNamespaceSchemaLocation="TreeTable.xsd" high="20" width="125" '
+        'color="black" detailable="true" standardmenu="true">'
+        '<TableData>'
+        '<RowData><name>出差（正常栏填0.1）</name><NodeData nodeid="T_1321_16645_4408" '
+        'nodetypeid="1321_16645_4408" image="29" />'
+        '<CellData><Cell colid="ck" type="boolean">True</Cell><Cell colid="t1" />'
+        '<Cell colid="t2" /></CellData></RowData>'
+        '</TableData></TreeTable>)')
+    params = {'startDate': startDate, 'endDate': endDate}
+    send_to_server('mywork/timesheet/addMyTaskAction.do', xml_data, "添加出差", params)
 
-def getHolidaysWeekOfDay(page):
+
+def get_week_of_day(desc):
+    week_of_day = {
+        u'星期一': '0',
+        u'星期二': '1',
+        u'星期三': '2',
+        u'星期四': '3',
+        u'星期五': '4',
+        u'星期六': '5',
+        u'星期天': '6',
+    }
+    return week_of_day.get(desc[:3])
+
+
+def get_holiday_week_of_day(page):
     result = set()
     for holiday in holidays:
-		 result.add(getWeekOfDay(page.find(name="td", text=re.compile(holiday))))
+        result.add(get_week_of_day(page.find(name="td", text=re.compile(holiday))))
     return result
 
-#查看报工页面
-def parsePage():
-	response = sendToServer('mywork/timesheet/initTimeSheetAction.do', base_form)
-	#查看出差一栏是否已添加:
-	if bizTravelID not in response.content:
-		addBizTravel()
-	#查看项目一栏是否添加
-	for projectName in projectNames:
-		if projectName not in response.content:
-			addProject(projectName)
-	
-	response = sendToServer('mywork/timesheet/initTimeSheetAction.do', base_form)
-	soup = BeautifulSoup(response.content, fromEncoding="GBK")
 
-	#查看是否配置文件如果有节假日日期，不填工时:
-	ignoreDays = getHolidaysWeekOfDay(soup)
+# 查看报工页面
+def parse_page():
+    global input_ids
+    response = send_to_server('mywork/timesheet/initTimeSheetAction.do', base_form)
+    # 查看出差一栏是否已添加:
+    if bizTravelID not in response.content:
+        add_biz_travel()
+    # 查看项目一栏是否添加
+    for project_name in project_names:
+        if project_name not in response.content:
+            add_project(project_name)
 
-	#根据之前添加的项目id和inputtext_timesheet获取输入框的id：
-	inputIDs = [x.get('name') for x in soup.findAll(name="input", attrs={"name": re.compile(projectID), "class" : re.compile("inputtext")})]
-	fillWorkHour(inputIDs, ignoreDays)
+    response = send_to_server('mywork/timesheet/initTimeSheetAction.do', base_form)
+    soup = BeautifulSoup(response.content, fromEncoding="GBK")
 
-	biz_travel_input_ids = [x.get('name') for x in soup.findAll(name="input", attrs={"name": re.compile(bizTravelID), "class" : re.compile("inputtext")})]
-	fill_travel_hour(biz_travel_input_ids, ignoreDays)
+    # 查看是否配置文件如果有节假日日期，不填工时:
+    ignore_days = get_holiday_week_of_day(soup)
 
-def filter_holiday(holidays_arg, input_ids, request_form):
+    # 根据之前添加的项目id和inputtext_timesheet获取输入框的id：
+    input_ids = [x.get('name') for x in soup.findAll(
+        name="input",
+        attrs={
+            "name": re.compile(projectID),
+            "class": re.compile("inputtext")})]
+
+    fill_work_hour(input_ids, ignore_days)
+
+    biz_travel_input_ids = [x.get('name') for x in soup.findAll(
+        name="input",
+        attrs={
+            "name": re.compile(bizTravelID),
+            "class": re.compile("inputtext")})]
+    fill_travel_hour(biz_travel_input_ids, ignore_days)
+
+
+def filter_holiday(holidays_arg, input_ids_arg, request_form):
     result = []
-    for input_id in input_ids:
+    for input_id in input_ids_arg:
         if input_id[-1] in holidays_arg:
             request_form[input_id] = ''
         else:
@@ -139,29 +164,32 @@ def filter_holiday(holidays_arg, input_ids, request_form):
             
     return result, request_form
 
-#根据项目数随机分配工时
-def decomposition(remainHour):
-	cnt = projectCnt
-	while cnt > 0:
-		if cnt == 1:
-			yield round(remainHour, 1)
-		else:
-			n = round(random.uniform(0, remainHour), 1)
-			yield n
-			remainHour -= n
-		cnt -= 1
 
-#获取最后一位字符
-def getLastChar(s):
-	return s[-1]
+# 根据项目数随机分配工时
+def decomposition(remain_hour):
+    cnt = projectCnt
+    while cnt > 0:
+        if cnt == 1:
+            yield round(remain_hour, 1)
+        else:
+            n = round(random.uniform(0, remain_hour), 1)
+            yield n
+            remain_hour -= n
+        cnt -= 1
 
-#获取工时
-def getHour(hours, index, workType):
+
+# 获取最后一位字符
+def get_last_char(string):
+    return string[-1]
+
+
+# 获取工时
+def get_hour(hours, index, work_type):
     origin_index = index
     if origin_index >= projectCnt:
         origin_index = 0
         index = 0
-        if workType == NORMAL_TYPE:
+        if work_type == NORMAL_TYPE:
             hours = list(decomposition(WORK_HOURS))
         else:
             hours = list(decomposition(OT_WORK_HOURS))
@@ -171,16 +199,18 @@ def getHour(hours, index, workType):
         hour = hours[origin_index]
     except IndexError:
         hour = ''
-    return (hours, hour, index)
+    return hours, hour, index
 
-#判断是否加班列
-def isOtWork(content):
-	if "gxot" in content:
-		return True
-	else:
-		return False
 
-#填写出差工时
+# 判断是否加班列
+def is_ot_work(content):
+    if "gxot" in content:
+        return True
+    else:
+        return False
+
+
+# 填写出差工时
 def fill_travel_hour(biz_travel_input_ids, holidays_arg=set()):
     biz_hours_map = {}
     biz_hours_map.update(base_form)
@@ -195,59 +225,61 @@ def fill_travel_hour(biz_travel_input_ids, holidays_arg=set()):
             pattern = re.compile('(' + bizTravelID + '.*)_')
             biz_hours_map['ptt'] = pattern.search(bizID).groups()[0]
 
-        if isOtWork(bizID):
+        if is_ot_work(bizID):
             biz_hours_map[bizID] = ''
         else:
             biz_hours_map[bizID] = '0.1'
-    response = sendToServer('mywork/timesheet/saveTimeSheetAction.do',  biz_hours_map)
+    send_to_server('mywork/timesheet/saveTimeSheetAction.do',  biz_hours_map)
 
-#填写普通报工
-def fillWorkHour(inputIDs, holidays=set()):
-    pttList = []
-    workHoursMap = {}
-    workHoursMap.update(base_form)
+
+# 填写普通报工
+def fill_work_hour(input_ids_arg, holidays_set=set()):
+    ptt_list = []
+    work_hours_map = {}
+    work_hours_map.update(base_form)
 
     cnt = 0
-    hourIndex = 0
-    otHourIndex = 0
+    hour_index = 0
+    othour_index = 0
     hours = list(decomposition(WORK_HOURS))
-    otHours = list(decomposition(OT_WORK_HOURS))
+    ot_hours = list(decomposition(OT_WORK_HOURS))
 
-    if holidays:
-        inputIDs, workHoursMap = filter_holiday(holidays, inputIDs, workHoursMap)
+    if holidays_set:
+        input_ids_arg, work_hours_map = filter_holiday(holidays_set, input_ids_arg, work_hours_map)
 
-    for inputID in sorted(inputIDs, key=getLastChar):
+    for inputID in sorted(input_ids_arg, key=get_last_char):
         if cnt < projectCnt:
-            if not isOtWork(inputID):
+            if not is_ot_work(inputID):
                 pattern = re.compile('(' + projectID + '.*)_')
-                pttList.append(pattern.search(inputID).groups()[0])
+                ptt_list.append(pattern.search(inputID).groups()[0])
                 cnt += 1
 
-        if cmp(inputID[-1], getWeekOfDay(u'星期六')) == 0 or cmp(inputID[-1], getWeekOfDay(u'星期天')) == 0:
-            if isOtWork(inputID):
-                (otHours, workHour, otHourIndex) = getHour(otHours, otHourIndex, NORMAL_TYPE)
+        if cmp(inputID[-1], get_week_of_day(u'星期六')) == 0 or cmp(inputID[-1], get_week_of_day(u'星期天')) == 0:
+            if is_ot_work(inputID):
+                (ot_hours, work_hour, othour_index) = get_hour(ot_hours, othour_index, NORMAL_TYPE)
             else:
-                workHour = ''
+                work_hour = ''
         else:
-            if isOtWork(inputID):
-                (otHours, workHour, otHourIndex) = getHour(otHours, otHourIndex, OT_TYPE)
+            if is_ot_work(inputID):
+                (ot_hours, work_hour, othour_index) = get_hour(ot_hours, othour_index, OT_TYPE)
             else:
-                (hours, workHour, hourIndex) = getHour(hours, hourIndex, NORMAL_TYPE)
+                (hours, work_hour, hour_index) = get_hour(hours, hour_index, NORMAL_TYPE)
 
-        workHoursMap[inputID] = workHour
+        work_hours_map[inputID] = work_hour
 
-    for ptt in pttList:
-        workHoursMap['ptt'] = ptt
-        response = sendToServer('mywork/timesheet/saveTimeSheetAction.do',  workHoursMap)
-    print response.content
+    for ptt in ptt_list:
+        work_hours_map['ptt'] = ptt
+        send_to_server('mywork/timesheet/saveTimeSheetAction.do',  work_hours_map)
+        # print response.content
 
-def openPageByIE():
+
+def open_page_by_ie():
     url = host + 'mywork/timesheet/saveTimeSheetAction.do' + '?startDate=' + str(startDate) + '&endDate=' + str(endDate)
-    PAMIE.openWorkHourPage(url)
+    PAMIE.open_work_hour_page(url)
 
 if __name__ == '__main__':
-    #getLastWeekDate()
-    #TEST
+    # get_last_week_date()
+    # TEST
     startDate = '2015-04-06'
     endDate = '2015-04-12'
     base_form = {'startDate': startDate, 'endDate': endDate}
@@ -255,7 +287,7 @@ if __name__ == '__main__':
     raw_input()
 
     login()
-    parsePage()
+    parse_page()
 
-    #打开报工页面人工核实提交
-    openPageByIE()
+    # 打开报工页面人工核实提交
+    open_page_by_ie()
