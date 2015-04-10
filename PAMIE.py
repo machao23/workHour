de288663e7acd012e353a2d9a7166dba74d86138
machao23 <274445541@qq.com>
2015-04-10 15:00:40 +0800

    遵守PEP8规范

--- PAMIE.py
+++ PAMIE.py
@@ -1,27 +1,15 @@
-#encoding=cp936
+# encoding=cp936
 import cPAMIE
 from time import sleep
 
-def openWorkHourPage(url):
-	ie = cPAMIE.PAMIE()
-
-	ie.navigate('http://vp.csii.com.cn/project/login.jsp')
-	ie.textBoxSet('loginName', '052321')
-	ie.textBoxSet('password', '052321')
-	ie.imageClick(0)
-	sleep(1)
-
-	ie.navigate(url)
-
-#	ie.frameName = "top"
-#	ie.listBoxSelect("mywork", u"我要报工")
-#	
-#	print "进入报工页面了吗"
-#	raw_input()
-#	ie.frameName = "right"
-#
-#	iframe = ie.getFrame('functionPage')
-#	print iframe.document
+def open_work_hour_page(url):
+    ie = cPAMIE.PAMIE()
+    ie.navigate('http://vp.csii.com.cn/project/login.jsp')
+    ie.textBoxSet('loginName', '052321')
+    ie.textBoxSet('password', '052321')
+    ie.imageClick(0)
+    sleep(1)
+    ie.navigate(url)
 
 if __name__ == '__main__':
-	openWorkHourPage()
+    open_work_hour_page()
