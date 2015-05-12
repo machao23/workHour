7fba06443d6ddd34786d435f9526528ba9387298
machao23 <274445541@qq.com>
2015-05-12 15:01:40 +0800

    登陆用户名和密码使用配置文件

--- PAMIE.py
+++ PAMIE.py
@@ -3,11 +3,11 @@ import cPAMIE
 from time import sleep
 
 
-def open_work_hour_page(url):
+def open_work_hour_page(url, login_name, pass_word):
     ie = cPAMIE.PAMIE()
     ie.navigate('http://vp.csii.com.cn/project/login.jsp')
-    ie.textBoxSet('loginName', '052321')
-    ie.textBoxSet('password', '052321')
+    ie.textBoxSet('loginName', login_name)
+    ie.textBoxSet('password', pass_word)
     ie.imageClick(0)
-    sleep(1)
-    ie.navigate(url)
+    # sleep(1)
+    # ie.navigate(url)
